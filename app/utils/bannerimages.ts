0898d48e73ca57ad8b05c8eb522c1036669e4ea0
Jojoe <webtendigital@gmail.com>
2022-10-25 11:01:13 +0300

    Testing fixed and sticky in dashboard

--- app/utils/bannerimages.ts
+++ app/utils/bannerimages.ts
@@ -1,5 +1,5 @@
-import vendorindex from '../images/banner/case-ya-muwala-2.jpg'
-import blogbanner from '../images/banner/case-ya-muwala-2.jpg'
+import vendorindex from '../images/banner/venue-banner.jpg'
+import blogbanner from '../images/banner/venue-banner.jpg'
 import cakebanner from '../images/banner/cake-banner.jpg'
 import venuebanner from '../images/banner/venue-banner.jpg'
 
